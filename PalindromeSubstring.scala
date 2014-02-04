7dc224bb1ada04946c99652e8d8138a56cbbde41
bekbolatov <renatbek@gmail.com>
2014-02-04 01:10:46 -0800

    implemented Manacher's algorithm

--- PalindromeSubstring.scala
+++ PalindromeSubstring.scala
@@ -8,8 +8,56 @@ object MaximalSubstringPalindromeFinder {
     ._1
   }
   def fastAlgorithm(a: String) : String = {
-    //re-using information
-    "Not implemented"
+    //implementing Manacher's algorithm, which re-uses a lot of information
+    val pn = 2*a.length - 1
+    if (pn < 2) return a
+    val p = Array.ofDim[Int](pn) //[p(0),a(0)] [p(1),] [p(2),a(1)] [p(3)] [p(4),a(2)] 
+    p(0) = 0
+    p(1) = if(a(0) == a(1)) 1 else 0
+    var maxIndex = 1
+    var maxValue = p(1)
+    var center = 1
+    var edge = 2
+    var check = 2
+    var anticheck = -1
+    for (check <- 2 to pn - 1) {
+      anticheck = 2*center - check
+      while(anticheck < 0) {
+        center += 1
+        anticheck = 2*center - check
+      }
+      edge = center + p(center)
+      if(edge - check > p(anticheck)) {
+        p(check) = p(anticheck)
+      } else {
+        center = check
+        var rem = edge - check
+        if (rem < 0) rem = 0
+        var sym = true
+        while(sym) {
+          if(center + rem + 1 < pn && center - rem - 1 >= 0) {
+            if((center + rem + 1) % 2 == 0) {
+              if(a((center + rem + 1)/2) == a((center - rem - 1)/2)) {
+                rem += 1
+              } else {
+                sym = false
+              }
+            } else {
+              rem += 1
+            }
+          } else {
+            sym = false
+          }
+        }
+        p(check) = rem
+        if (maxValue < rem) {
+          maxValue = rem 
+          maxIndex = check
+        }
+      }
+    }
+    maxValue -= maxValue%2
+    a.substring((maxIndex - maxValue)/2, (maxIndex + maxValue)/2+1)
   }
 }
 
@@ -23,11 +71,23 @@ object PalindromeSubstring {
   def main(args: Array[String]) {
     if (args.length == 0) {
       println("supply a string as a command-line argument")
+      println("For example, try following command:")
+      println("scala PalindromeSubstring 98jnibfgfgfgfg453bdgdfgdgdfudf" +
+        "gdfgdfgdfgdfgy7565f5frerexrex16559234329545498jhhgdfgfgfg089fg" +
+        "fhgfdsasdfghf1234567890232323232dfdfdfererertyuiuytreasdfghjkl" +
+        "kjhgfdsaqwertyuioplkjhgfdfghjklpoiuytrewq");
     } else {
       val m = MaximalSubstringPalindromeFinder;
+      var x: String = ""
+      val output = "Result=%s (%d ms, input length %d, method: %s)\n"
+
       resetTimer()
-      val x = m.slowAlgorithm(args(0))
-      printf("Answer \"%s\" is found in %d ms for a string of length %d checking each substring\n", x, args(0).length, curTime)
+      x = m.slowAlgorithm(args(0))
+      printf(output, x, curTime, args(0).length, "check each substring")
+
+      resetTimer()
+      x = m.fastAlgorithm(args(0))
+      printf(output, x, curTime, args(0).length, "Manacher's algorithm")
     }
   }  
 }
--- README.md
+++ README.md
@@ -1,4 +1,25 @@
 PalindromeSubstringScala
 ========================
 
-goal is to write maximal palindrome substring in functional programming
+Two algorithms are provided:
+
+1. One is using a slower approach - basically a brute-force try of every substring:
+O(n^2) in string comparison operations, which are typically O(n), making a total of O(n^3).
+
+2. Another approach is to re-use as much information as possible: Manacher's algorightm.
+Here we are traversing the candidate palindrome centers once, using symmetry and knowledge
+about previously expanded palindromes (code should be self-explanatory) - 
+achieves linear time on average. 
+Worst-case situation is when the string is made out of same character: aaaaaaaaa...
+Here algorithm would have to expand to maximum at each center, making it O(n^2).
+
+Check out the benefit:
+
+    scala PalindromeSubstring 98jnibfgfgfgfg453bdgdfgdgdfudfgdfgdfgdfgdfgy7565f5frerexrex16559234329545498jhhgdfgfgfg089fgfhgfdsasdfghf1234567890232323232dfdfdfererertyuiuytreasdfghjklkjhgfdsaqwertyuioplkjhgfdfghjklpoiuytrewq
+Result=qwertyuioplkjhgfdfghjklpoiuytrewq (512 ms, input length 195, method: check each substring)
+Result=qwertyuioplkjhgfdfghjklpoiuytrewq (5 ms, input length 195, method: Manacher's algorithm)
+
+
+In future, I might make the second algorithm implementation more Scala idiomatic, more FP
+
+
